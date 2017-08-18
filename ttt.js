0639e2bad0539c323cd1bd1ca0550634529931c4
Kathleen <kathleenmcclay@gmail.com>
2017-08-17 21:28:23 -0400

    First Commit HTML foundation complete, Basic CSS and Javascript squares light up

---
+++ ttt.js
@@ -0,0 +1,64 @@
+document.addEventListener('DOMContentLoaded', function (){
+
+var square1 = document.querySelector('.one')
+var square2 = document.querySelector('.two')
+
+
+var turn = 'X';
+var score = {
+    'X': 0,
+    'O': 0
+};
+
+
+
+
+  square1.addEventListener('click', function(){
+var square1 = document.querySelector('.one').style.backgroundColor = "blue"
+  });
+
+  square2.addEventListener('click', function(){
+var square2 = document.querySelector('.two').style.backgroundColor = "blue"
+  });
+
+  square3.addEventListener('click', function(){
+  var square3 = document.querySelector('.two').style.backgroundColor = "blue"
+  });
+
+  square4.addEventListener('click', function(){
+var square4 = document.querySelector('.two').style.backgroundColor = "blue"
+  });
+
+  square5.addEventListener('click', function(){
+var square5 = document.querySelector('.two').style.backgroundColor = "blue"
+  });
+
+  square6.addEventListener('click', function(){
+var square6 = document.querySelector('.two').style.backgroundColor = "blue"
+  });
+
+  square7.addEventListener('click', function(){
+var square7 = document.querySelector('.two').style.backgroundColor = "blue"
+  });
+
+  square8.addEventListener('click', function(){
+var square8 = document.querySelector('.two').style.backgroundColor = "blue"
+  });
+
+  square9.addEventListener('click', function(){
+  var square9 = document.querySelector('.two').style.backgroundColor = "blue"
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
